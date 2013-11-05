f78a063c9065f53befabc07a2c7085150b1bfaf7
delihiros <delihiros@gmail.com>
2013-11-05 22:19:29 +0900

    refactoring

--- src/kancolle_clj/core.clj
+++ src/kancolle_clj/core.clj
@@ -38,6 +38,24 @@
 (def deck (create-api-call "/api_get_member/deck"))
 (def deck-port (create-api-call "/api_get_member/deck_port"))
 
+  (defn get-fleets-ids []
+    (map #(clojure.string/join "," (:api_ship %))
+         (:api_data_deck (get-ship2 :api_sort_order 2 :api_sort_key 1))))
+    
+  (defn get-damaged-ships []
+    (sort-by #(/ (:api_nowhp %) (:api_maxhp %))
+             (filter #(and (not= (:api_nowhp %) (:api_maxhp %))  
+                           (not-any? #{(:api_id %)} (map  :api_ship_id (:api_data (get-ndock)))))
+                     (:api_data (get-ship)))))
+
+  (defn charge-fleet [fleet]
+    (charge :api_kind 3, :api_id_items fleet))
+
+  (defn recover-ship [ship ndock_id]
+    (start-recovery :api_ship_id (:api_id ship) :api_ndock_id ndock_id :api_highspeed 0))
+
+
+
 (comment
   (clojure.pprint/pprint (let [result (:api_data (get-ship))]
                            (sort-by #(nth % 2)
@@ -63,21 +81,11 @@
       (get-basic)
       (charge-fleet fleet)))
 
-  (defn get-fleets-ids []
-    (map #(clojure.string/join "," %)
-         (map :api_ship (:api_data_deck (get-ship2 :api_sort_order 2 :api_sort_key 1)))))
 
-  (defn get-damaged-ships []
-    (sort-by #(/ (:api_nowhp %) (:api_maxhp %))
-             (filter #(and (not= (:api_nowhp %) (:api_maxhp %))  
-                           (not-any? #{(:api_id %)} (map  :api_ship_id (:api_data (get-ndock)))))
-                     (:api_data (get-ship)))))
+(map :api_name (get-damaged-ships))
 
-  (defn charge-fleet [fleet]
-    (charge :api_kind 3, :api_id_items fleet))
   (map charge-fleet (get-fleets-ids))
 
+  (recover-ship (first (get-damaged-ships)) 2)
 
-  (defn recover-ship [ship ndock_id]
-    (start-recovery :api_ship_id (:api_id ship) :api_ndock_id ndock_id :api_highspeed 0))
-  )
+)
