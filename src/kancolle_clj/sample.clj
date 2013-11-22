18ee9263a66de3dd82a8cd232751e55ee593b551
delihiros <delihiros@gmail.com>
2013-11-22 21:18:04 +0900

    refactoring done

--- src/kancolle_clj/sample.clj
+++ src/kancolle_clj/sample.clj
@@ -3,47 +3,52 @@
         [kancolle-clj.wrapper]
         [kancolle-clj.api]))
 
-;; 第一艦隊を自動で回復-> 3-2-1
-;; (leveling-fleet 1)
-
 (defn leveling-fleet [fleet-number]
   (let [fleet (nth (get-fleets-ids) (- fleet-number 1))]
     (charge-fleet fleet)
     (battle-start :api_formation_id 1 :api_deck_id fleet-number :api_mapinfo_no 2 :api_maparea_id 3)
-    (battle :api_formation fleet-number)
+    (battle :api_formation 1)
     (battle-result)
-    (get-ship2 :api_sort_order 2 :api_sort_key 1)
+    (ship2 :api_sort_order 2 :api_sort_key 1)
     (slotitem)
     (deck)
-    (login-check)
-    (get-material)
+    (logincheck)
+    (material)
     (deck-port)
-    (get-ndock)
-    (get-ship2 :api_sort_order 2 :api_sort_key 1)
-    (get-basic)
+    (ndock)
+    (ship2 :api_sort_order 2 :api_sort_key 1)
+    (basic)
     (charge-fleet fleet)))
-
-(leveling-fleet 2)
 
 (defn get-devtool-by [fleet-number]
   (let [fleet (nth (get-fleets-ids) (- fleet-number 1))]
     (charge-fleet fleet)
-    (mission-start :api_deck_id 3 :api_mission_id 2)
+    (mission-start :api_deck_id fleet-number :api_mission_id 2)
     (deck)))
 
-(comment
-  (charge-fleet (nth (get-fleets-ids) 2))
-  (get-devtool-by 3)
-  (mission-result :api_deck_id 3)
-  )
+(defn get-bauxite-by [fleet-number]
+  (let [fleet (nth (get-fleets-ids) (- fleet-number 1))]
+    (charge-fleet fleet)
+    (mission-start :api_deck_id fleet-number :api_mission_id 15)
+    (deck)))
 
-(clojure.pprint/pprint
-  (recover-ship
-    (first
-      (filter (fn [ship]
-                (and (not= (:api_nowhp ship) (:api_maxhp ship))
-                     (not-any? #{(:api_id ship)}
-                               (map :api_ship_id (:api_data (get-ndock))))))
-              (sort-by
-                (fn [ship] (/ (:api_nowhp ship) (:api_maxhp ship)))
-                (:api_data (get-ship))))) 1))
+(defn recover-most-damaged-ship []
+  (let [ndock-status (ndock)
+                     empty-docks (map :api_id (filter (fn [dock] (= (:api_state dock) 0)) (:api_data (ndock))))]
+    (if (not (zero? (count empty-docks)))
+      (recover-ship
+        (first
+          (filter (fn [ship]
+                    (and (not= (:api_nowhp ship) (:api_maxhp ship))
+                         (not-any? #{(:api_id ship)}
+                                   (map :api_ship_id (:api_data (ndock))))))
+                  (sort-by
+                    (fn [ship] (/ (:api_nowhp ship) (:api_maxhp ship)))
+                    (:api_data (ship))))) (first empty-docks)))))
+
+(comment
+  (for [fleet-id [2 3 4]]
+    (leveling-fleet fleet-id))
+  (get-devtool-by 3)
+  (get-bauxite-by 4)
+  )
